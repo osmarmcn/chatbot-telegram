a212ffd720fd864269038b98bfaa508ab828751b
osmarmcn <99884631+osmarmcn@users.noreply.github.com>
2024-08-12 19:13:40 -0300

    ajuste final

--- app3.py
+++ app3.py
@@ -1,52 +1,47 @@
 
 
-# 7339844257:AAExECWw4_UWhwxXU67VjMaf1nxAKUdesXM
-
-
-# 7339844257:AAExECWw4_UWhwxXU67VjMaf1nxAKUdesXM
-
-# https://web.telegram.org/a/#7176261902
 
 
 
 
-import requests  # Importa a biblioteca requests para fazer solicitações HTTP
-import os  # Importa o módulo os para manipulação de funcionalidades dependentes do sistema operacional
 
-class TelegramBot:  # Define uma classe chamada TelegramBot
-    def __init__(self):  # Define o método inicializador da classe
-        self.token = '7496061516:AAFK89P9dz_Q2izyTxWAgWZIEvx4IPHimow'  # Define o token do bot do Telegram
-        self.url_base = f'https://api.telegram.org/bot{self.token}/'  # Define a URL base da API do Telegram usando o token
-        self.menu = {  # Define um dicionário com o menu do restaurante
+import requests 
+import os  
+
+class TelegramBot: 
+    def __init__(self):  
+        self.token = '7496061516:AAFK89P9dz_Q2izyTxWAgWZIEvx4IPHimow'  
+        self.url_base = f'https://api.telegram.org/bot{self.token}/'  
+        self.menu = {  
             '1': {'nome': 'FabzBurguer Classic', 'preco': 20.00},
             '2': {'nome': 'FabzCheeseBacon Burguer', 'preco': 25.00},
             '3': {'nome': 'FabzTriploBMT Burguer', 'preco': 30.00},
             '4': {'nome': 'FabzFries', 'preco': 15.00}
         }
-        self.pedidos = {}  # Inicializa um dicionário vazio para armazenar os pedidos dos clientes
+        self.pedidos = {} 
 
-    def iniciar(self):  # Define o método para iniciar o bot
-        update_id = None  # Inicializa o ID da atualização como None
-        while True:  # Loop infinito para ficar constantemente verificando por novas mensagens
-            updates = self.obter_mensagens(update_id)  # Obtém as mensagens mais recentes
-            mensagens = updates['result']  # Extrai as mensagens do resultado da atualização
-            for mensagem in mensagens:  # Loop pelas mensagens recebidas
-                update_id = mensagem['update_id']  # Obtém o ID da atualização para evitar processar a mesma mensagem novamente
-                chat_id = mensagem['message']['from']['id']  # Obtém o ID do chat de onde a mensagem veio
-                resposta = self.criar_resposta(mensagem, chat_id)  # Cria uma resposta com base na mensagem recebida
-                self.responder(resposta, chat_id)  # Envia a resposta para o chat de origem da mensagem
+    def iniciar(self):  
+        update_id = None  
+        while True:  
+            updates = self.obter_mensagens(update_id)  
+            mensagens = updates['result']  
+            for mensagem in mensagens: 
+                update_id = mensagem['update_id']  
+                chat_id = mensagem['message']['from']['id']  
+                resposta = self.criar_resposta(mensagem, chat_id)  
+                self.responder(resposta, chat_id) 
 
-    def obter_mensagens(self, update_id):  # Define o método para obter as mensagens do Telegram
-        link_requisicao = f'{self.url_base}getUpdates?timeout=100'  # Define a URL da requisição para obter as atualizações
-        if update_id:  # Se houver um ID de atualização, adiciona o parâmetro offset para obter apenas novas atualizações
+    def obter_mensagens(self, update_id):  
+        link_requisicao = f'{self.url_base}getUpdates?timeout=100'  
+        if update_id:  
             link_requisicao = f'{link_requisicao}&offset={update_id + 1}'
-        resultado = requests.get(link_requisicao)  # Faz uma solicitação GET para obter as atualizações
-        return resultado.json()  # Retorna o conteúdo da resposta da solicitação como um objeto JSON
+        resultado = requests.get(link_requisicao)  
+        return resultado.json()  
 
-    def criar_resposta(self, mensagem, chat_id):  # Define o método para criar uma resposta com base na mensagem recebida
-        mensagem_texto = mensagem['message']['text']  # Extrai o texto da mensagem
+    def criar_resposta(self, mensagem, chat_id):  
+        mensagem_texto = mensagem['message']['text']  
         if mensagem_texto.lower() in ('oi', 'ola', 'tudo bem?') or mensagem_texto.lower() == 'menu' or mensagem_texto.lower() == '/start':
-            # Se a mensagem for uma saudação, solicitação de menu ou comando /start, retorna o menu
+         
             return f'''Olá, bem-vindo ao FabzBurguer!{os.linesep}
                       Por favor, escolha uma de nossas belezuras abaixo:{os.linesep}
                       1 - FabzBurguer Classic{os.linesep}
@@ -56,60 +51,60 @@ class TelegramBot:  # Define uma classe chamada TelegramBot
                       5 - Verificar pedido.'''
         
         elif mensagem_texto in self.menu.keys():
-            # Se a mensagem corresponder a uma opção de menu, confirma o pedido
+           
             return self.confirmar_pedido(mensagem_texto, chat_id)
         
         elif mensagem_texto.lower() in ('s', 'sim') and chat_id in self.pedidos:
-            # Se a mensagem for uma confirmação de pedido e houver um pedido registrado para o chat, solicita o método de pagamento
+            
             return 'Qual será o método de pagamento? (Cartão de Crédito / Pix)'
         
         elif mensagem_texto.lower() in ('cartão', 'cartão de crédito', 'crédito', 'credito', 'cartao') and chat_id in self.pedidos:
-            # Se o método de pagamento for cartão de crédito e houver um pedido registrado para o chat, confirma o pedido
+           
             self.pedidos[chat_id]['confirmado'] = True
             return 'Pedido confirmado! Seu pedido foi armazenado.'
         
         elif mensagem_texto.lower() == 'pix' and chat_id in self.pedidos:
-            # Se o método de pagamento for Pix e houver um pedido registrado para o chat, confirma o pedido
+          
             self.pedidos[chat_id]['confirmado'] = True
             return 'Pedido confirmado! Seu pedido foi armazenado.'
         
         elif mensagem_texto.lower() == '5':
-            # Se a mensagem for para verificar o pedido, retorna os pedidos do cliente
+           
             return self.verificar_pedido(chat_id)
 
         else:
-            # Se a mensagem não for reconhecida, solicita ao cliente
+           
             return 'Desculpe, não entendi. Gostaria de acessar o menu? Digite "menu".'
 
     def confirmar_pedido(self, item, chat_id):
-    # Verifica se o chat_id não está registrado nos pedidos e, se não estiver, cria uma entrada para ele
+   
         if chat_id not in self.pedidos:
             self.pedidos[chat_id] = {'itens': [], 'confirmado': False}
-        # Adiciona o item selecionado aos pedidos do chat_id
+      
         self.pedidos[chat_id]['itens'].append({'nome': self.menu[item]['nome'], 'preco': self.menu[item]['preco']})
-        # Retorna uma mensagem confirmando o pedido e solicitando o método de pagamento
+       
         return f'''Pedido confirmado! Por favor, confirme o método de pagamento para o item 
         {self.menu[item]["nome"]} - R${self.menu[item]["preco"]:.2f} (Cartão de Crédito ou Pix)'''
 
     def verificar_pedido(self, chat_id):
-        # Verifica se o chat_id está registrado nos pedidos e se o pedido está confirmado
+       
         if chat_id in self.pedidos and self.pedidos[chat_id]['confirmado']:
-            pedidos_usuario = self.pedidos[chat_id]['itens']  # Obtém os pedidos do usuário
-            if len(pedidos_usuario) > 0:  # Se houver pedidos registrados
-                mensagem = 'Seus pedidos:'  # Inicia a mensagem com os pedidos do usuário
+            pedidos_usuario = self.pedidos[chat_id]['itens']  
+            if len(pedidos_usuario) > 0:  
+                mensagem = 'Seus pedidos:'  
                 for i, pedido in enumerate(pedidos_usuario, start=1):
-                    # Para cada pedido, adiciona o nome e o preço formatados à mensagem
+                   
                     mensagem += f'\n{i}. {pedido["nome"]} - R${pedido["preco"]:.2f}'
-                return mensagem  # Retorna a mensagem com os pedidos do usuário
+                return mensagem 
             else:
-                return 'Você não tem pedidos registrados.'  # Se não houver pedidos registrados, retorna uma mensagem informando isso
+                return 'Você não tem pedidos registrados.' 
         else:
-            return 'Seu pedido não foi confirmado'  # Se o pedido não foi confirmado, retorna uma mensagem informando isso
+            return 'Seu pedido não foi confirmado'  
 
     def responder(self, resposta, chat_id):
-        # Constrói o link para enviar a mensagem de resposta usando a API do Telegram
+       
         link = f'{self.url_base}sendMessage?chat_id={chat_id}&text={resposta}'
-        # Envia a mensagem de resposta para o chat especificado
+     
         requests.get(link)
 
 bot = TelegramBot()
